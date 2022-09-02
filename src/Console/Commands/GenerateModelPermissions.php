77347b250768a2f0b314885252242a116ac6f2b4
Loek Wetzels <loek.wetzels@gmail.com>
2022-09-02 16:28:33 +0200

    artisan commands now accept a --model option

--- src/Console/Commands/GenerateModelPermissions.php
+++ src/Console/Commands/GenerateModelPermissions.php
@@ -1,69 +1,162 @@
 <?php
+/**
+ * @format
+ */
 
 namespace Eightbitsnl\PolicyAndPermissionGenerator\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Spatie\Permission\Models\Permission;
 
 class GenerateModelPermissions extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'permissions:generate';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = "permissions:generate {--model= : Specify a model}";
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create new permissions, based on the App models';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = "Create new permissions, based on the App models";
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  private function findModelClasses($path = null)
+  {
+    $path = is_null($path)
+      ? (is_dir(app_path("Models"))
+        ? app_path("Models")
+        : app_path(""))
+      : $path;
+
+    return collect(File::allFiles($path))
+      ->map(function ($path) {
+        if (File::isDirectory($path)) {
+          return $this->findModelClasses($path);
+        }
+
+        if (File::extension($path) == "php") {
+          return "App" .
+            str_replace("/", "\\", substr($path, strlen(app_path()), -4)) .
+            "";
+        }
+      })
+      ->flatten()
+      ->filter(function ($classname) {
+        return is_subclass_of($classname, Model::class);
+      })
+      ->values();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    $models = $this->findModelClasses();
+
+    $input_model = $this->option("model");
+
+    if (!is_null($input_model)) {
+      if ($models->contains($input_model)) {
+        $selected = [$input_model];
+      } else {
+        if (
+          class_exists($input_model) &&
+          is_subclass_of($input_model, Model::class)
+        ) {
+          $models->push($input_model);
+          $selected = [$input_model];
+        } else {
+          $this->error("Model not found.");
+        }
+      }
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        
+    $selected =
+      $selected ??
+      $this->choice(
+        "Create Permissions for which models? Use , to select multiple: 1,2,3",
+        $models->toArray(),
+        null,
+        null,
+        true
+      );
 
-        $abilities = collect(['create','view','update','delete','viewAny','restore', 'forceDelete']);
+    $abilities = collect([
+      "create",
+      "view",
+      "update",
+      "delete",
+      "viewAny",
+      "restore",
+      "forceDelete",
+    ]);
 
-        // find all files in /app folder 
-        collect(File::files(app_path()))
-            ->map(function ($item) use($abilities) {
+    // create Permission for selected classes
+    collect($selected)
+      ->map(function ($fqcn) {
+        return class_basename($fqcn);
+      })
+      ->crossJoin($abilities)
+      ->each(function ($group) {
+        $permission_name = implode(".", $group);
 
-                // model basename
-                $basename = substr($item->getBasename(), 0, -4);
+        $permission = Permission::firstOrCreate(["name" => $permission_name]);
 
-                return $abilities->map(function($ability) use ($basename){
-                    return $basename.'.'.$ability;
-                });
+        if ($permission->wasRecentlyCreated) {
+          $this->info("- Permission " . $permission_name . " created");
+        } else {
+          $this->line("- Permission " . $permission_name . " skipped");
+        }
+      });
 
-            })
-            ->flatten()
-            ->each(function($permission_name){
-                $permission = Permission::firstOrCreate(['name' => $permission_name]);
+    // detect Policies
+    $stub_path = __DIR__ . "/Stubs/ModelPolicy.stub";
+    if (is_file($stub_path)) {
+      $policiesDir = app_path("Policies");
+      $stub = file_get_contents($stub_path);
+      collect($selected)->each(function ($fqcn) use ($policiesDir, $stub) {
+        $policy_class_name = class_basename($fqcn) . "Policy";
+        $targetPath = $policiesDir . "/" . $policy_class_name . ".php";
 
-                if($permission->wasRecentlyCreated)
-                    $this->info('- Permission '.$permission_name. ' created');
-                else
-                    $this->line('- Permission '.$permission_name. ' skipped');
-                    
-            });
+        if (!file_exists($targetPath)) {
+          if ($this->confirm("No Policy found for `$fqcn` Create?")) {
+            $replace = [
+              "DummyModelFqcn" => $fqcn,
+              "DummyModelBasename" => class_basename($fqcn),
+              "DummyPolicyClass" => $policy_class_name,
+            ];
+
+            $contents = str_replace(
+              array_keys($replace),
+              array_values($replace),
+              $stub
+            );
+
+            file_put_contents($targetPath, $contents);
+            $this->info("- Policy " . $policy_class_name . " created");
+          }
+        }
+      });
     }
+  }
 }
--- src/Console/Commands/GenerateModelPolicies.php
+++ src/Console/Commands/GenerateModelPolicies.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * @format
+ */
 
 namespace Eightbitsnl\PolicyAndPermissionGenerator\Console\Commands;
 
@@ -8,71 +11,74 @@ use Illuminate\Support\Facades\File;
 
 class GenerateModelPolicies extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'policies:generate';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = "policies:generate {--model= : Specify a model}";
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create new Policy classes, based on the App models';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = "Create new Policy classes, based on the App models";
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-     
-        $stub = file_get_contents(__DIR__ . '/../Stubs/policy.stub');
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    $stub = file_get_contents(__DIR__ . "/../Stubs/policy.stub");
 
+    $targetDir = app_path("Policies");
 
-        $targetDir =  app_path('Policies');
-    
-        // find all models
-        collect(File::files(app_path()))
-            ->each(function ($item) use ($stub, $targetDir){
+    // find all models
+    $models_dir = is_dir(app_path("Models")) ? app_path("Models") : app_path();
 
-                // model basename
-		        $model_name = substr($item->getBasename(), 0, -4);
-                $policy_name = $model_name.'Policy';
+    $models = $this->option("model")
+      ? collect($this->option("model"))
+      : collect(File::glob($models_dir . "/*.php"))->map(function ($str) {
+        return substr($str, 0, -4);
+      });
 
-                $targetPath = $targetDir .'/' . $policy_name . '.php';
+    $models->each(function ($path) use ($stub, $targetDir) {
+      // model basename
+      $model_name = class_basename($path);
+      $policy_name = $model_name . "Policy";
 
+      $targetPath = $targetDir . "/" . $policy_name . ".php";
 
-                if (! file_exists($targetPath) )
-                {
-                    $replace = [
-                        'DummyNamespace' => 'App\Policies',
-                        'DummyFullModelClass' => 'App\\' . $model_name,
-                        'DummyModelClass' => $model_name,
-                        'DummyClass' => $policy_name
-                    ];
+      if (!file_exists($targetPath)) {
+        $replace = [
+          "DummyNamespace" => "App\Policies",
+          "DummyFullModelClass" => "App\\" . $model_name,
+          "DummyModelClass" => $model_name,
+          "DummyClass" => $policy_name,
+        ];
 
-                    $contents = str_replace(
-                        array_keys($replace), array_values($replace), $stub
-                    );
+        $contents = str_replace(
+          array_keys($replace),
+          array_values($replace),
+          $stub
+        );
 
-                    file_put_contents($targetPath, $contents);
-                    $this->info('- Policy '. $policy_name. ' created');
-                }
-
-            });
-    }
+        file_put_contents($targetPath, $contents);
+        $this->info("- Policy " . $policy_name . " created");
+      }
+    });
+  }
 }
